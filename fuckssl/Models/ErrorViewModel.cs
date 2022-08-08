fe1d67eb0954ba5c255e47965f502e4b18f2cb38
Cem <cem.kurt@nextlua.com>
2022-08-08 08:18:28 +0300

    Add project files.

---
+++ fuckssl/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fuckssl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
